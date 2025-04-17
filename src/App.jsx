cdf90831c822edee1a947f8dff338c9e9ddfc27e
“jaideep-vs” <test@gmail.comgit>
2025-04-17 13:35:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom"
+import "./css/App.css"
+import Home from "./pages/Home.jsx"
+import Favorites from "./pages/Favorites.jsx"
+import { MovieProvider } from "./contexts/MovieContext.jsx"
+import NavBar from "./components/NavBar.jsx"
+function App() {
+  return (
+<MovieProvider>
+  <NavBar/>
+<main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/favorites" element={<Favorites/>}/>
+      </Routes>
+    </main>
+</MovieProvider>
+  )
+}
+
+export default App
